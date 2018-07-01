1d3d1459719b8c5dd272534b76a0fa2d26fa01d2
Anirudh Muhnot <anirudhmuhnot@Anirudhs-MacBook-Air.local>
2018-07-01 20:59:06 +0530

    a

--- projects/allProjects.py
+++ projects/allProjects.py
@@ -3,6 +3,10 @@ import dash_html_components as html
 import dash_core_components as dcc
 import base64
 
+@app.server.route('/static/<path:path>')
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'static')
+    return send_from_directory(static_folder, path)
 
 #future database variables
 projects = {
