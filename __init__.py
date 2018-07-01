860c8e868b9030445a081ba4f06aa55287b01cd8
Anirudh Muhnot <anirudhmuhnot@Anirudhs-MacBook-Air.local>
2018-07-01 21:20:22 +0530

    a

--- __init__.py
+++ __init__.py
@@ -4,14 +4,15 @@ import dash_html_components as html
 import dash
 from flask import send_from_directory
 import time
-import base64
+from projects import home,allProjects,student_dash,crime_analysis
+
 app = dash.Dash(static_folder='static')
 app.config.supress_callback_exceptions = True
 
 external_css = [
     'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    '/static/base1.css'
+    '/var/www/FlaskApp/FlaskApp/static/base1.css'
 ]
 
 for css in external_css:
@@ -25,38 +26,12 @@ for my_js in external_js:
   app.scripts.append_script({"external_url": my_js})
 
 
-@app.server.route('/static/<path:path>')
+@app.server.route('/var/www/FlaskApp/FlaskApp/static/<path:path>')
 def static_file(path):
     static_folder = os.path.join(os.getcwd(), 'static')
     return send_from_directory(static_folder, path)
 
-#future database variables
-projects = {
-            'Student Dashboard': ['A dashboard for tracking attendance, academic performance of students in realtime.','student_dash'], 
-            'Crime Data Analysis': ['Analysis of IPSC crime committed from year 2000-2014.','crime_analysis']
-           };
-a = []
-for key,value in projects.iteritems():
-        img_name = '/static/'+value[1]+'.png'
-        a.append(html.Div(className='col s6 m4 l4',children=[
-                html.Div(className='card small',children=[
-                    html.Div(className='card-image waves-effect waves-block waves-light',children=[
-                        html.Img(className='activator',src='data:image/jpg;base64,{}'.format(base64.b64encode(open(img_name,'rb').read())))
-                    ]),
-                    html.Div(className='card-content',children=[
-                        html.Span(className='card-title activator grey-text text-darken-4',children=[key,html.I(className='material-icons right',children=['more_vert'])]),
-                        html.P(dcc.Link(html.A('View'),href='/projects/'+value[1]))
-                    ]),
-                    html.Div(className='card-reveal grey darken-1 white-text',children=[
-                        html.Span(className='card-title grey-text text-darken-4',children=[
-                        key,html.I(className='material-icons right',children=['more_vert'])
-                        ]),
-                        html.P(value[0])
-                    ])  
-                ])
-            ])
-        )
-from projects import home,allProjects,student_dash,crime_analysis
+
 #apps_database
 apps = {
   'home':home,
