894a6d320502d62b563715b051ff471e6378985e
Anirudh Muhnot <anirudhmuhnot@Anirudhs-MacBook-Air.local>
2018-07-02 00:04:57 +0530

    a

--- projects/crime_analysis.py
+++ projects/crime_analysis.py
@@ -1,7 +1,37 @@
 from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
+#dependencies
+import pandas as pd #dataframe manipulation
+import plotly.plotly as py #visualisation
+import plotly.figure_factory as ff
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.graph_objs as go
+from plotly.grid_objs import Grid, Column
+import time
+import numpy as np #numerical computations
+import cufflinks as cf #plotly with pandas
+import plotly.tools as tls
+
+
+df = pd.read_csv('/var/www/FlaskApp/FlaskApp/projects/crime_data/01_District_wise_crimes_committed_IPC_2001_2012.csv')
 
 layout = html.Div(children=[
-    html.H1('Crime Analysis')
+    html.H1('IPC Crime Analysis'),
+    dcc.Graph(
+		figure = {
+	    	'data' : [go.Bar(
+	            x=df['STATE/UT'],y=df['TOTAL IPC CRIMES'],name='TOTAL IPC CRIMES',marker=dict(color='#F3558E')
+	    	)],
+	    	'layout' : {
+		        'title':'NUMBER OF CRIMES COMMITTED IN DIFFERENT STATES',
+		        'font':dict(size=12, color='#7f7f7f'),
+		        'titlefont':dict(size=24,family='Courier New, monospace'),
+		        'height':620,
+		        'margin':go.Margin(b=130,pad=3),
+		        'xaxis':dict(title='State',tickfont=dict(size=10)),
+		        'yaxis':dict(title='Number Of Crimes Committed')
+    		}
+		}
+    )
 ])
