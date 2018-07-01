7ec486d74db884149739b72faf33b516a4faae53
Anirudh Muhnot <anirudhmuhnot@Anirudhs-MacBook-Air.local>
2018-07-01 20:21:23 +0530

    a

--- __init__.py
+++ __init__.py
@@ -87,4 +87,4 @@ def display_page(pathname):
 
 server = app.server
 if __name__ == '__main__':
-    app.run_server(debug=True,host=0.0.0.0,port=9601)
+    app.run_server(debug=True,host='0.0.0.0',port=9601)
